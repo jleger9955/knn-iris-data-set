c32e18ab3b9336686a55002af0f3e99e202fe455
erajoc7 <Justin.Leger@McKesson.com>
2019-02-15 14:56:52 -0600

    Updating knn code

--- knn.py
+++ knn.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Knn:
 
     def __init__(self):
@@ -12,5 +15,14 @@ class Knn:
         lengths = [(vector.scale(-1) + v).norm() for v in self.vectors]
         lenspec = [(length, self.species[i]) for i, length in enumerate(lengths)]
         final = sorted(lenspec, key=lambda t: t[0])[:k]
-
-        return final
+        prediction = final[0][1]
+        ''' The code below is only needed if you expect more than one species in the "final" list variable
+        count_spec = defaultdict(int)
+        for spc in final:
+            count_spec[spc[1]] += 1
+        prediction = ''
+        for spc, cnt in count_spec.items():
+            if count_spec[spc] >= 2:
+                prediction = spc
+        '''
+        return prediction
--- main.py
+++ main.py
@@ -11,13 +11,12 @@ with open('data/train.csv') as f:
         model.train(vector, species)
 
 experiments = []
-with open('data/train.csv') as f:
+with open('data/test.csv') as f:
     for line in f:
         data = line.strip().split(',')
         vector = Vector(*map(float, data[:4]))
         species = data[-1]
         prediction = model.predict(vector, 3)
-        print(prediction)
         experiments.append(species == prediction)
 accuracy = sum(experiments) / len(experiments)
-print('accuracy:', accuracy)
+print('accuracy: {:.2%}'.format(accuracy))
